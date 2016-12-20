99874e8c48308ec876b8324d3344d452bdc085c9
llwoll <hailiangwin@gmail.com>
2016-12-20 22:32:33 +0800

    update server release

--- app/src/main/java/com/llwoll/navigation/utils/Config.java
+++ app/src/main/java/com/llwoll/navigation/utils/Config.java
@@ -6,6 +6,9 @@ package com.llwoll.navigation.utils;
 
 public class Config {
 
-    public static String SEVERHOST = "http://192.168.58.1:8081";
+//    public static String SEVERHOST = "http://192.168.58.1:8081";
+
+      public static String SEVERHOST = "http://112.74.35.232:8080";
+
 
 }
